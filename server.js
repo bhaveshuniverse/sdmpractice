0caf3320f8deaf71dd583fcf717dbf996ff912ba
unknown <BHAVESH KUMAR VERMA>
2023-01-02 15:06:06 +0530

    done

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+var app=express();
+
+var path=require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(8080);
+console.log("Server is running on port No 8080");
